bfe33778adc49214cce2e79b7a454a15bb630ba0
jeffmerkey <jeffmerkey@gmail.com>
2023-01-22 21:55:54 -0800

    fix spelling
    Signed-off-by: jeffmerkey <jeffmerkey@gmail.com>

--- example.php
+++ example.php
@@ -1,20 +1,6 @@
 <?php
 include "pdf417-class.php";
 /*
-   It's important to disable the shortcut keys Ctrl-J and Ctrl-D if using
-   the 2D scanner in Chrome or the scanning process will cause the browser to 
-   jump into either the Downloads screen (Ctrl-J) or the bookmark dropdown 
-   dialog (Ctrl-D).  Download the chrome extension from Chrome Store
-   called "disable keyboard shortcuts" then after it installs, type
-   "chrome://extensions/shortcuts" in the url bar and hit enter.  Enter
-   Ctrl-J and Ctrl-D into the fields in the extension to disable these 
-   shortcuts and your 2D bar code scanner should work properly.  Firefox 
-   maps Ctrl-J to the dropdown url dialog and unlike chrome, there is no 
-   way to easily disable shortcut keys which may be in a PDF417 data stream.  
-   This behavior occurs because a bar code scanner basically works like a
-   keyboard in how it interfaces to a web browser.
-*/
-/*
    Sample scan data for illustration purposes.  Normally this type of
    datastream is what gets returned from a 2D scanner when you scan
    the pdf417 2D barcode on the back of the id or driver's license
